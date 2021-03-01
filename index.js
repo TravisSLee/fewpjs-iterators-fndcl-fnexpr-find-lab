3da44e3c1d6482014c739d8c1ae7643531c417b1
TravisSLee <travisslee223@gmail.com>
2021-03-01 13:59:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ] 
+function superbowlWin(array){
+    let win = array.find(function(e) { return e.result === "W" })
+    return win['year']
+};
